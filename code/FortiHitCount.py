06af1e021b8f3c548373418e7a32e7d53e02e221
Michael W. Norris <42365063+norrismw@users.noreply.github.com>
2019-09-10 16:21:48 -0600

    Update FortiHitCount.py

--- code/FortiHitCount.py
+++ code/FortiHitCount.py
@@ -3,15 +3,16 @@
 
 import sys
 import re
-import time
+import csv
 from netmiko import ConnectHandler
+from forticonf import fortigate
 
-
-## used regardless of choice; used in gen_commands() and get_count()
+## used regardless of choice; used in gen_commands(), is_disabled(), and get_count()
 # generates list of policies
 
 def get_policies():
     policy_string = net_connect.send_command('show firewall policy | grep edit')
+    #print('a') ##
     return [int(s) for s in policy_string.split() if s.isdigit()]
 
 
@@ -29,9 +30,12 @@ def gen_commands():
 # sends the list of generated clear commands
 
 def clear_count():
+    print('[!] Clearing hit counts ...')
     for command in gen_commands():
-        print(command)
-        # net_connect.send_command(command)
+        print('') ##
+        #net_connect.send_command(command)
+    return 0
+
 
 ## used if choice == "show"; used in clean_list()
 # generates messy list from output of show commands; used in clean_list()
@@ -39,7 +43,7 @@ def clear_count():
 def messy_list():
     messy_list = []
     for command in gen_commands():
-        time.sleep(1)
+        #print('b') ##
         messy_list.append(net_connect.send_command(command))
     return messy_list
 
@@ -59,30 +63,49 @@ def clean_list():
     return clean_list
 
 
+## used if choice == "show"; used in get_count()
+# generates dictionary of policies and disabled statuses
+
+def is_disabled():
+    print('[*] Determining which policies are disabled ...')
+    disabled_dict = dict.fromkeys(get_policies())
+    for policy_key in disabled_dict:
+        #print('c') ##
+        if net_connect.send_command('show firewall policy ' + str(policy_key) + ' | grep "set status disable"') != '':
+            disabled_dict[policy_key] = 'disabled'
+        else:
+            disabled_dict[policy_key] = 'enabled'
+    return disabled_dict
+
+
 ## used if choice == "show"
-# combines the keys (policy ids) with the hit counts (for each policy) into a dict
+# combines the keys (policy ids) with the hit counts (for each policy) into a dictionary
 
 def get_count():
+    print('[!] Generating dictionary ...')
     keys = get_policies()
     values = clean_list()
     hit_counts = dict(zip(keys, values))
+    disabled_dict = is_disabled()
+    for policy_key in disabled_dict:
+        if disabled_dict[policy_key] == 'disabled':
+            hit_counts[policy_key] = 'disabled'
     return hit_counts
 
 
-# this will be a configuration file in practice
+## used if cohice == "show"
+# writes hit_counts dictionary into csv file called hit_counts.csv
 
-fortigate = {
-    'device_type': 'fortinet',
-    'host': '',
-    'username': '',
-    'password': '',
-    'port': 22, # optional, defaults to 22
-    'secret': '', # optional, defaults to ''
-    }
+def write_csv():
+    hit_counts = get_count()
+    with open('hit_counts.csv', 'w') as f:
+        for key in hit_counts.keys():
+            f.write("%s,%s\n"%(key,hit_counts[key]))
 
-choice = sys.argv[1]
 
 # logic to determine which commands should be generated/sent
+
+choice = sys.argv[1]
 
 if choice == 'clear':
     base_command = 'di firewall iprope clear 100004'
@@ -91,11 +114,19 @@ elif choice == 'show':
 else:
     sys.exit(1)
 
+print('[*] Using base command: ' + base_command)
+
+print('[*] Connecting ...')
 net_connect = ConnectHandler(**fortigate)
+print('[+] Successfully connected.')
 
 if choice == 'clear':
     clear_count()
 else:
-    get_count()
+    write_csv()
 
+print('[*] Disconnecting ...')
 net_connect.disconnect()
+print('[+] Success!')
+
+sys.exit(0)
